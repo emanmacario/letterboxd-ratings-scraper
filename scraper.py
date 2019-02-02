7fe85347d716d057acedba1ec4e7ec5f23edaece
EmanMacario <emacario1997@hotmail.com>
2019-02-02 13:41:41 +1100

    Switched to films page to make processing super fast

--- scraper.py
+++ scraper.py
@@ -8,21 +8,23 @@
 
 import csv
 import re
+import os
 import sys
+import urllib.request
+import urllib.error
 from math import ceil
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 
 BASE_URL = 'https://letterboxd.com/'
-RATINGS_PATH = '/films/ratings/page/'
-DRIVER_PATH = r'C:\Users\Allan\Desktop\Python\chromedriver.exe'
-LOG_PATH = r'D:\chromedriver.log'
-RATINGS_PER_PAGE = 18
+RATINGS_PATH = '/films/page/'
+DRIVER_PATH = os.path.join(os.getcwd(), 'chromedriver.exe')
+RATINGS_PER_PAGE = 72
 
 # TODO: Experiment with PhantomJS or Headless Chrome instead of normal Chrome
 # TODO: Write scraped output to CSV file
@@ -33,6 +35,7 @@ def main():
     username = input("Enter username: ")
 
     validate_user_existence(username)
+    browser = initialise_browser()
 
     # Calculate total number of pages to be scraped
     total_pages = calc_total_pages(browser, username)
@@ -45,13 +48,14 @@ def main():
     browser.close()
 
 
-def initialise_browser(desired_capabilities=None):
-    if desired_capabilities is None:
-        return webdriver.Chrome(DRIVER_PATH)
-    else:
-        return webdriver.Chrome(executable_path=DRIVER_PATH,
-                                service_args=["--verbose", "--log-path=" + LOG_PATH],
-                                desired_capabilities=desired_capabilities)
+def initialise_browser():
+    # Operate Chrome in headless mode
+    chrome_options = Options()
+    chrome_options.headless = False
+
+    # Initialise and return driver
+    return webdriver.Chrome(executable_path=DRIVER_PATH,
+                            chrome_options=chrome_options)
 
 
 def validate_user_existence(username):
@@ -59,28 +63,17 @@ def validate_user_existence(username):
         print("Please enter a non-empty username")
         sys.exit(0)
     else:
-        # Enable browser logging
-        desired_capabilities = DesiredCapabilities.CHROME
-        print(desired_capabilities)
-        desired_capabilities['loggingPrefs'] = {'performance': 'ALL'}
-
-        browser = initialise_browser(desired_capabilities)
-
-        browser.get(BASE_URL + username)
-
-        print(browser.title)
-
-        print(browser.page_source)
-
-        performance_log = browser.get_log('performance')
-        print(str(performance_log).strip('[]'))
-
-        for entry in browser.get_log('performance'):
-            print(entry)
-
-        browser.close()
-
-        browser.get(BASE_URL + username)
+        try:
+            urllib.request.urlopen(BASE_URL + username)
+        except urllib.error.HTTPError as e:
+            # Return code error (e.g. 404, 501, ...)
+            print('HTTPError: {}'.format(e.code))
+        except urllib.error.URLError as e:
+            # Not an HTTP-specific error (e.g. connection refused)
+            print('URLError: {}'.format(e.reason))
+        else:
+            # 200
+            print('User has been found')
 
 
 def calc_total_pages(browser, username):
@@ -89,11 +82,13 @@ def calc_total_pages(browser, username):
     given the user's total number of film ratings.
     :return:
     """
+    browser.get(BASE_URL + username)
     ratings_section = browser.find_element_by_xpath('//section[@class="section ratings-histogram-chart"]')
     print(ratings_section)
 
     total_ratings = int(ratings_section.find_element_by_class_name('all-link')
-                                       .text.replace(',', ''))
+                                       .text
+                                       .replace(',', ''))
     print(total_ratings)
 
     total_pages = ceil(total_ratings / RATINGS_PER_PAGE)
@@ -111,23 +106,28 @@ def scrape_page_ratings(browser):
     film_ratings = browser.find_elements_by_class_name('poster-container')
     print("Total elements:", len(film_ratings))
 
+    # Regular expression for film title and year
+    pattern = re.compile(r"(?P<title>.*) \((?P<year>\d{4})\)")
+
     for film in film_ratings:
+        sys.stdout.flush()
+        print("Next film...")
         try:
+            print("Trying to hover...")
             # Locate the mouse-over element
             hover = ActionChains(browser).move_to_element(film)
             hover.perform()
 
+            print("Hover successful, waiting for dynamic element")
             # Get the dynamic element
-            element = WebDriverWait(browser, 30).until(
+            element = WebDriverWait(browser, 5).until(
                 expected_conditions.presence_of_element_located((By.CLASS_NAME, "twipsy-inner"))
             )
 
-            # Regular expression for film title and year
-            pattern = re.compile(r"(?P<title>.*) \((?P<year>\d{4})\)")
-
+            print("Dynamic element found, finding match")
             # Try to find a match
             match = pattern.match(element.text)
-            if match:
+            if match is not None:
                 title = match.group('title')
                 year = match.group('year')
                 print(title, year)
@@ -135,13 +135,13 @@ def scrape_page_ratings(browser):
         except TimeoutException:
             print("Error, loading dynamic element took too much time!")
 
-
         # Parse rating and title in scope of child elements
         # title = film.find_element_by_xpath('.//img').get_property('alt')
         # print("TITLE:", title)
 
-        rating = film.find_element_by_xpath('.//meta[@itemprop="ratingValue"]')\
-                     .get_property('content')
+        # rating = film.find_element_by_xpath('.//meta[@itemprop="ratingValue"]')
+        #             .get_property('content')
+        rating = film.get_attribute('data-owner-rating')
         print("RATING:", rating)
 
         letterboxd_uri = BASE_URL + film.find_element_by_xpath('.//div[1]')\
